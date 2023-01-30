91d648d25a9918d7724eeb27b58c16246b98cdca
vloss <viniciusvianaloss@gmail.com>
2023-01-30 18:01:20 -0300

    Iniciando projeto.

---
+++ Campeões da Copa/Campeões da Copa/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Campeões da Copa
+//
+//  Created by Vinicius Loss on 30/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
